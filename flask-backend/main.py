ef30fd8b11bb69822b34eb805391c7c9f6209f80
liang303 <stuffbyliang@gmail.com>
2020-03-08 00:03:01 -0800

    connected /song endpoint to find all rhymes

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,9 +1,17 @@
-import flask
+from flask import request, Flask, jsonify
+from song import Song
+import json
 
-app = flask.Flask("__main__")
+app = Flask("__main__")
 
 @app.route("/")
 def my_index():
     return flask.render_template("index.html", token="hello world")
+
+@app.route("/song", methods=['GET'])
+def song():
+    lyrics = request.args.get('lyrics')
+    song = Song(lyrics)
+    return json.dumps(song.find_all_rhyme_clusters())
 
 app.run(debug=True)
