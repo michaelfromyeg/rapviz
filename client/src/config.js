d61621ccb1d8cf75549a681b4d1e9f52d31ab32b
Michael DeMarco <michaelfromyeg@gmail.com>
2023-06-18 12:39:29 -0700

    build: deploy frontend; set endpoints appropriately

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,12 @@
-export const serverEndpoint = "http://localhost:5000";
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+
+export const serverEndpoint = isDevelopment ? "http://localhost:5000" : "https://rapviz-production.up.railway.app";
 
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "9f19d3f83c3a493cadd7f4cf1ab8661d";
-export const redirectUri = "http://localhost:3000/";
+export const redirectUri = isDevelopment ? "http://localhost:3000/" : "https://michaelfromyeg.github.io/rapviz/";
 export const scopes = [
   "user-top-read",
   "user-read-currently-playing",
